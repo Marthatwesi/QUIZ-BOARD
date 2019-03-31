e5c870106e0cc98412bb11c1d96caa382b5b4ac5
twesigyemartha <twesimartha@gmail.com>
2019-03-31 15:52:47 +0300

    started editting the js file now

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+$("input[type='button']").click(function(){
+//create an array for storing name ie quiz1 to quiz10
+  arr = [];
+  for (i = 1; i <= 10; i++) {
+  query = "quiz"+i;
+  arr.push(query);
+  }
